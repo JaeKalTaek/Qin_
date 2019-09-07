075b20c60b7177b97dbb476ddf4c0246da6028fc
Divi <benjaminr@laposte.net>
2019-09-07 15:48:59 +0200

    Pop up menus are centered on Y axis to avoid them going off screen

--- Qin/Assets/Scripts/OutOfGame/UI/SC_PopUp_Menu.cs
+++ Qin/Assets/Scripts/OutOfGame/UI/SC_PopUp_Menu.cs
@@ -10,7 +10,7 @@ public class SC_PopUp_Menu : MonoBehaviour {
 
         int offset = currentTileViewportPos.x < 0.5 ? 1 : -1;
 
-        Rect.anchorMin = new Vector3(currentTileViewportPos.x + (offset * (0.1f + (0.05f * (1 / (Mathf.Pow(Camera.main.orthographicSize, Camera.main.orthographicSize / 4)))))), currentTileViewportPos.y, currentTileViewportPos.z);
+        Rect.anchorMin = new Vector3(currentTileViewportPos.x + (offset * (0.1f + (0.05f * (1 / (Mathf.Pow(Camera.main.orthographicSize, Camera.main.orthographicSize / 4)))))), .5f, 0);
         Rect.anchorMax = Rect.anchorMin;
 
     }

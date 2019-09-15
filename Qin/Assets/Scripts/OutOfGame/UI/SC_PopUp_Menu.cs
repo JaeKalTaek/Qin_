5269b43e0f7b21443b108c1795ca474e7205364f
Divi <benjaminr@laposte.net>
2019-09-15 15:10:26 +0200

    Minor refactoring

--- Qin/Assets/Scripts/OutOfGame/UI/SC_PopUp_Menu.cs
+++ Qin/Assets/Scripts/OutOfGame/UI/SC_PopUp_Menu.cs
@@ -8,9 +8,7 @@ public class SC_PopUp_Menu : MonoBehaviour {
 
         Vector3 currentTileViewportPos = Camera.main.WorldToViewportPoint(SC_Cursor.Instance.transform.position);
 
-        int offset = currentTileViewportPos.x < 0.5 ? 1 : -1;
-
-        Rect.anchorMin = new Vector3(currentTileViewportPos.x + (offset * (0.1f + (0.05f * (1 / (Mathf.Pow(Camera.main.orthographicSize, Camera.main.orthographicSize / 4)))))), .5f, 0);
+        Rect.anchorMin = new Vector3(currentTileViewportPos.x + ((currentTileViewportPos.x < 0.5 ? 1 : -1) * (0.1f + (0.05f * (1 / (Mathf.Pow(Camera.main.orthographicSize, Camera.main.orthographicSize / 4)))))), .5f, 0);
         Rect.anchorMax = Rect.anchorMin;
 
     }
